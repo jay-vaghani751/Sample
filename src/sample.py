17322432d268c3cf2eb7c7dafd0641b14d5a1625
Jay Vaghani <jai.vaghani5682@gmail.com>
2025-02-23 18:13:12 -0500

    Initial commit: Project structure setup

---
+++ src/sample.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import pandas as pd
+print(pd.array([1, 2, 3]))
+print(" Visual Studio Setup is Working Correctly!")
+print(f"Python Interpreter Path: {sys.executable}")
+print(f"Current Working Directory: {os.getcwd()}")
+input("\nPress Enter to exit...")
